0e762a212f0ae89bbfac6e99beaeb910240da318
DESKTOP-UV3LARI\azisulyagifari <azisulyagifari27@gmail.com>
2020-11-15 22:27:38 +0700

    update

--- quiz-2/main-2.js
+++ quiz-2/main-2.js
@@ -17,7 +17,7 @@
 
       methods: {
         addName () {
-          if(this.newName != '') {
+          if(this.newName != ' ') {
             this.list.push({name: this.newName});
             this.newName= ' ';
           } 
@@ -27,27 +27,18 @@
         },
         deleteName (data) {
           let result = confirm("Are you sure you want to delete?");
-          console.log(data)
-          console.log(this.list)
           if(result){
             this.list.splice(data,1)
           }
         },
         editName (data, desc) {
-          console.log(data)
-          console.log(desc)
           this.updateSubmit = true
           this.idName = data
           this.newName = desc
         },
         updateName(data, desc){
-          console.log("edit",data)
-          console.log("edit",desc)
-          let panjang = this.list.length;
-          console.log(panjang)
           this.list[data].name = desc 
           this.newName= ' ';
-          console.log(this.list)
         }
 
   
